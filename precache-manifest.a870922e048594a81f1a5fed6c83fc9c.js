825d029e231e8e865da6d6bce1301405735b4751
oleg5913 <oleg1906@gmail.com>
2020-11-01 03:01:04 +0300

    v.1.0

---
+++ precache-manifest.a870922e048594a81f1a5fed6c83fc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d00db8116b1bcce1058aca1d2034dca",
+    "url": "/social_network_demo/index.html"
+  },
+  {
+    "revision": "8a29beef058c185e6235",
+    "url": "/social_network_demo/static/css/main.d2e1716d.chunk.css"
+  },
+  {
+    "revision": "d4c5f55382c358904030",
+    "url": "/social_network_demo/static/js/2.02384eed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_network_demo/static/js/2.02384eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a29beef058c185e6235",
+    "url": "/social_network_demo/static/js/main.a150dcee.chunk.js"
+  },
+  {
+    "revision": "906a866eb2c365287846",
+    "url": "/social_network_demo/static/js/runtime-main.e9ebbd67.js"
+  }
+]);
